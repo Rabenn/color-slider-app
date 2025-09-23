931e241019c7f84b80be99fc7e73646e0a151bc4
Raben <rubenxioluna@gmail.com>
2025-09-23 19:55:49 +0200

    CHORE: Cambios principales y creacion del programa

--- src/main/java/es/ruben/App.java
+++ src/main/java/es/ruben/App.java
@@ -2,37 +2,21 @@ package es.ruben;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) throws Exception {
+        // Ahora el FXML está en resources/fxml/
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/color.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Using Slider Controls");
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
---
+++ src/main/java/es/ruben/ColorController.java
@@ -0,0 +1,37 @@
+package es.ruben;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class ColorController {
+
+    @FXML
+    private Rectangle colorBox;
+
+    @FXML
+    private Slider sliderRed;
+
+    @FXML
+    private Slider sliderGreen;
+
+    @FXML
+    private Slider sliderBlue;
+
+    @FXML
+    private void initialize() {
+        updateColor();
+
+        sliderRed.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        sliderGreen.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        sliderBlue.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+    }
+
+    private void updateColor() {
+        int r = (int) sliderRed.getValue();
+        int g = (int) sliderGreen.getValue();
+        int b = (int) sliderBlue.getValue();
+        colorBox.setFill(Color.rgb(r, g, b));
+    }
+}
